1ac1f3a5b98dac57343ee1e5c92c804b4343a56b
mayasaurusrex <maya.hegde64@gmail.com>
2023-12-23 17:45:24 -0700

    Add Podfile to create new packages
    - Charts: To create line graph shown in GraphViewController
    - Firestore: taken off since new XCode can support this

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '16.0'
+
+target 'Glaucoma_App' do
+	use_frameworks!
+
+pod 'Charts'
+end
---
+++ Pods/Target Support Files/Pods-Glaucoma_App/Pods-Glaucoma_App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Glaucoma_App : NSObject
+@end
+@implementation PodsDummy_Pods_Glaucoma_App
+@end
